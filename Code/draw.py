4f4470f4b9487dbd9919fdbc8713124b534d79aa
Sammy K. <kazbrekker898@gmail.com>
2025-10-23 17:16:23 +0530

    made changes to the menu file for better accessibility

--- Code/draw.py
+++ Code/draw.py
@@ -8,7 +8,7 @@ myList = os.listdir(folderPath)
 overlay = []
 for imPath in myList:
     image = cv.imread(f'{folderPath}/{imPath}')
-    overlay.append(cv.resize(image, (150, 720)))
+    overlay.append(image)
 header = overlay[0]
 
 wcam, hcam = 1280, 720
@@ -25,7 +25,7 @@ fingList = [8,12,16,20]
 while True:
     ret, img = video.read()
     img = cv.flip(img, 1)
-    img[0: 720, 0:150] = header
+    img[0: 100, 0:1280] = header
     h, w, c = img.shape
     
     imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
@@ -34,6 +34,7 @@ while True:
     if results.multi_hand_landmarks:
         for handIdx, hl in enumerate(results.multi_hand_landmarks):
             lmdic = {}
+            fingers = []
             for (i, lm) in enumerate(hl.landmark):
                 lmdic[i] = [lm.x*w, lm.y*h]
 
@@ -44,15 +45,44 @@ while True:
             if handLabel == 'Right':
                 if lmdic[4][0] < lmdic[3][0]:
                     cv.circle(img, (cx, cy), 15, (0,0,225), 5)
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
             else:
                 if lmdic[4][0] > lmdic[3][0]:
                     cv.circle(img, (cx, cy), 15, (0,0,225), 5)
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
 
             for i in fingList:
                 fx = int(lmdic[i][0])
                 fy = int(lmdic[i][1])
                 if fy < lmdic[i-2][1]:
                     cv.circle(img, (fx, fy), 15, (0,0,225), 5)
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+
+            if fingers[1] == 1 and fingers[2] == 1:
+                #selection mode
+                x1, y1 = lmdic[8][0], lmdic[8][1]
+                x2, y2 = lmdic[12][0], lmdic[12][1]
+                if y1 < 150:
+                    if 140<x1<425:
+                        header = overlay[0]
+                    elif 425<x1<710:
+                        header = overlay[1]
+                    elif 710<x1<995:
+                        header = overlay[2]
+                    elif 995<x1<1280:
+                        header = overlay[3]
+                    else:
+                        continue
+            
+            if fingers[1] == 1 and fingers[2] == 0:
+                #drawing mode
+                pass
                 
             mpDraw.draw_landmarks(img, hl, mpHands.HAND_CONNECTIONS)
     
