1374d934a2361200f5f93a40c1079449e35fc272
Sammy K. <kazbrekker898@gmail.com>
2025-10-30 00:15:55 +0530

    Upgraded the garment section
    added tshirt pngs for the sake of garment tryout instead of using polygon filling. The sizing and the orientation is bit off but it all works considering that it's still in the testing phase.

--- Code/draw.py
+++ Code/draw.py
@@ -3,14 +3,29 @@ import mediapipe as mp
 import numpy as np
 import time
 import os
+import json
 
-folderPath = "D:\Smart-Mirror\Menu"
+folderPath = r"D:\Smart-Mirror\Menu"
+tshirtPath = r"D:\Smart-Mirror\assets"
+
 myList = os.listdir(folderPath)
-overlay = []
-for imPath in myList:
-    image = cv.imread(f'{folderPath}/{imPath}')
-    overlay.append(image)
-header = overlay[0]
+tList = os.listdir(tshirtPath)
+
+overlayTop = []
+overlayBottom = []
+
+for i in range(len(myList)):
+    image = cv.imread(f'{folderPath}/{myList[i]}')
+    overlayTop.append(image)
+
+    timage = cv.imread(f'{tshirtPath}/{tList[i]}', cv.IMREAD_UNCHANGED)
+    overlayBottom.append(timage)
+
+header = overlayTop[0]
+garment = overlayBottom[0]
+
+if garment is None:
+    raise ValueError("Error: Could not load the T-shirt image. Check the file path.")
 
 wcam, hcam = 1280, 720
 video = cv.VideoCapture(0)
@@ -21,52 +36,75 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 pT = 0
-fingList = [8,12,16,20]
+fingList = [8, 12, 16, 20]
 
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
-tshirtColor = (0,0,225)
+tshirtColor = (0, 0, 225)
 
 while True:
     ret, img = video.read()
     img = cv.flip(img, 1)
-    img[0: 100, 0:1280] = header
+    img[0:100, 0:1280] = header
     h, w, c = img.shape
-    
+
     imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     resultsH = hands.process(imgRGB)
     resultsP = pose.process(imgRGB)
 
     if resultsP.pose_landmarks:
-        
         landmarksP = resultsP.pose_landmarks.landmark
 
         rightS, leftS = landmarksP[12], landmarksP[11]
         rightH, leftH = landmarksP[24], landmarksP[23]
 
-        rsx, rsy = int(rightS.x*w), int(rightS.y*h)
-        lsx, lsy = int(leftS.x*w), int(leftS.y*h)
-        rhx, rhy = int(rightH.x*w), int(rightH.y*h)
-        lhx, lhy = int(leftH.x*w), int(leftH.y*h)
+        rsx, rsy = int(rightS.x * w), int(rightS.y * h)
+        lsx, lsy = int(leftS.x * w), int(leftS.y * h)
+        rhx, rhy = int(rightH.x * w), int(rightH.y * h)
+        lhx, lhy = int(leftH.x * w), int(leftH.y * h)
 
-        # torsoWidth = (((lsx - rsx)**2)+((lsy-rsy)**2))**0.5
-        # torsoHeight = (((lsx - rhx)**2)+((lsy-rhy)**2))**0.5
+        torsoWidth = int((((lsx - rsx) ** 2) + ((lsy - rsy) ** 2)) ** 0.5) * 2
+        torsoHeight = int((((lsx - rhx) ** 2) + ((lsy - rhy) ** 2)) ** 0.5) * 2
 
-        # resizedGarment = cv.resize(garment, (torsoWidth, torsoHeight))
+        scale_w, scale_h = 1.4, 1.7
+        tshirt_width = int(torsoWidth * scale_w)
+        tshirt_height = int(torsoHeight * scale_h)
 
-        pts = np.array([[rsx, rsy], [lsx, lsy], [lhx, lhy], [rhx, rhy]], np.int32)
-        cv.fillPoly(img, [pts], tshirtColor)
+        center_x = (lsx + rsx) // 2
+        top_y = max(lsy, rsy) - 20
+        x1 = center_x - torsoWidth // 2
+        y1 = top_y - int(torsoHeight * 0.075)
 
-        # cv.rectangle(img, (lsx,lsy), (rhx,rhy), tshirtColor, -1)
-        mpDraw.draw_landmarks(img,  resultsP.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        # Resize and overlay
+        if torsoWidth > 0 and torsoHeight > 0:
+            tshirt_resized = cv.resize(garment, (torsoWidth, torsoHeight))
 
+            # Boundary checks
+            x1 = max(0, x1)
+            y1 = max(0, y1)
+            x2 = min(w, x1 + torsoWidth)
+            y2 = min(h, y1 + torsoHeight)
+
+            roi = img[y1:y2, x1:x2]
+            tshirt_cropped = tshirt_resized[0:y2 - y1, 0:x2 - x1]
+
+            # Alpha blending
+            if tshirt_cropped.shape[2] == 4:
+                alpha_t = tshirt_cropped[:, :, 3] / 255.0
+                alpha_b = 1.0 - alpha_t
+
+                for c in range(3):
+                    roi[:, :, c] = alpha_t * tshirt_cropped[:, :, c] + alpha_b * roi[:, :, c]
+
+                img[y1:y2, x1:x2] = roi
 
     if resultsH.multi_hand_landmarks:
         for handIdx, hl in enumerate(resultsH.multi_hand_landmarks):
             lmdic = {}
             fingers = []
+
             for (i, lm) in enumerate(hl.landmark):
-                lmdic[i] = [lm.x*w, lm.y*h]
+                lmdic[i] = [lm.x * w, lm.y * h]
 
             handLabel = resultsH.multi_handedness[handIdx].classification[0].label
             cx = int(lmdic[4][0])
@@ -81,52 +119,52 @@ while True:
                 if lmdic[4][0] > lmdic[3][0]:
                     fingers.append(1)
                 else:
-                    fingers.append(0)   
+                    fingers.append(0)
 
             for i in fingList:
                 fx = int(lmdic[i][0])
                 fy = int(lmdic[i][1])
-                if fy < lmdic[i-2][1]:
+                if fy < lmdic[i - 2][1]:
                     fingers.append(1)
                 else:
                     fingers.append(0)
 
             if fingers[1] == 1 and fingers[2] == 1:
-                #selection mode
+                # selection mode
                 x1, y1 = int(lmdic[8][0]), int(lmdic[8][1])
                 x2, y2 = int(lmdic[12][0]), int(lmdic[12][1])
-                cv.circle(img, (x1, y1), 15, (0,0,225), 3)
-                cv.circle(img, (x2, y2), 15, (0,0,225), 3)
+                cv.circle(img, (x1, y1), 15, (0, 0, 225), 3)
+                cv.circle(img, (x2, y2), 15, (0, 0, 225), 3)
+
                 if y1 < 150:
-                    if 140<x1<425:
-                        header = overlay[0]
-                        tshirtColor = (0,0,225)
-                    elif 425<x1<710:
-                        header = overlay[1]
-                        tshirtColor = (225,0,225)
-                    elif 710<x1<995:
-                        header = overlay[2]
-                        tshirtColor = (0,225,225)
-                    elif 995<x1<1280:
-                        header = overlay[3]
-                        tshirtColor = (225,225,225)
+                    if 140 < x1 < 425:
+                        header = overlayTop[0]
+                        garment = overlayBottom[0]
+                    elif 425 < x1 < 710:
+                        header = overlayTop[1]
+                        garment = overlayBottom[1]
+                    elif 710 < x1 < 995:
+                        header = overlayTop[2]
+                        garment = overlayBottom[2]
+                    elif 995 < x1 < 1280:
+                        header = overlayTop[3]
+                        garment = overlayBottom[3]
                     else:
                         continue
-            
+
             if fingers[1] == 1 and fingers[2] == 0:
-                #drawing mode
+                # drawing mode
                 pass
-                
-            # mpDraw.draw_landmarks(img, hl, mpHands.HAND_CONNECTIONS)
-    
+
     cT = time.time()
-    fps = 1/(cT-pT)
+    fps = 1 / (cT - pT)
     pT = cT
 
-    cv.putText(img, str(int(fps)), (15,40), cv.FONT_HERSHEY_COMPLEX, 1.5, (255,255,0), 2)
-
+    cv.putText(img, str(int(fps)), (15, 40), cv.FONT_HERSHEY_COMPLEX, 1.5, (255, 255, 0), 2)
     cv.imshow('Video', img)
-    if cv.waitKey(1) & 0XFF == ord('q'):
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
         break
+
 video.release()
 cv.destroyAllWindows()
